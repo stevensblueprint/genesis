838b27134fc370ba0a131594ee1e82ea4374cebb
Ivan Farfan <ivanfarfan567@yahoo.com>
2025-03-29 21:04:11 -0400

    feat: added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { Authenticator } from "./components/Authenticator"
 import "@aws-amplify/ui-react/styles.css"
 import { Button } from "antd"
 import { useAuthenticator } from "@aws-amplify/ui-react"
+import ProjectSelector from "./pages/ProjectSelector"
 
 const App = () => {
   const { user, signOut } = useAuthenticator((context) => [context.user]);
   return (
     <>
       <Authenticator>
-        Book
+        <ProjectSelector />
         <Button type="primary" onClick={signOut}>Logout</Button>
       </Authenticator>
     </>
---
+++ src/pages/ProjectSelector.tsx
@@ -0,0 +1,12 @@
+const ProjectSelector = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-red-500">
+        Test
+      </h1>
+    </div>
+  );
+};
+
+export default ProjectSelector;
+
