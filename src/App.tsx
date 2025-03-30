5bbc5fb9e12494b7d764d9ee08039e8d988988d8
Ivan Farfan <ivanfarfan567@yahoo.com>
2025-03-29 20:39:45 -0400

    feat: added authenticator

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Authenticator } from "./components/Authenticator"  
+import "@aws-amplify/ui-react/styles.css"
+import { Button } from "antd"
+import { useAuthenticator } from "@aws-amplify/ui-react"
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const { user, signOut } = useAuthenticator((context) => [context.user]);
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Authenticator>
+        Book
+        <Button type="primary" onClick={signOut}>Logout</Button>
+      </Authenticator>
     </>
   )
 }
--- src/components/Authenticator.tsx
+++ src/components/Authenticator.tsx
@@ -4,10 +4,40 @@ import {
   ThemeProvider as AmplifyThemeProvider,
   Theme,
 } from "@aws-amplify/ui-react";
-
+import logo from "../assets/logo_banner.png";
 export const Authenticator = ({ children }: PropsWithChildren) => {
   const theme: Theme = {
     name: "genesis",
+    tokens: {
+      colors: {
+        primary: {
+          10: "#f0f7fe",
+          20: "#cce5fc",
+          40: "#66b3f9",
+          60: "#0078e8",
+          80: "#005db5",
+          100: "#004282"
+        },
+        neutral: {
+          10: "#f5f5f5",
+          20: "#e6e6e6",
+          40: "#cccccc",
+          60: "#999999",
+          80: "#666666",
+          100: "#333333"
+        }
+      },
+      components: {
+        button: {
+          primary: {
+            backgroundColor: "#0078e8",
+            _hover: {
+              backgroundColor: "#005db5"
+            }
+          }
+        },
+      }
+    }
   };
 
   return (
@@ -15,6 +45,46 @@ export const Authenticator = ({ children }: PropsWithChildren) => {
       <AmplifyAuthenticator
         variation="modal"
         signUpAttributes={["given_name", "family_name"]}
+        formFields={{
+          signIn: {
+            username: {
+              label: "Email",
+              placeholder: "Enter your email",
+            },
+          },
+          signUp: {
+            username: {
+              label: "Email",
+              placeholder: "Enter your email",
+            },
+            given_name: {
+              label: "First Name",
+              placeholder: "Enter your first name",
+            },
+            family_name: {
+              label: "Last Name",
+              placeholder: "Enter your last name",
+            },
+          },
+        }}
+        components={{
+          Header : () => <>
+          <div style={{
+            display: "flex",
+            justifyContent: "center",
+            background: "#fff",
+            padding: "1rem",
+            borderRadius: "1rem 1rem 0 0"
+          }}>
+            <img 
+              src={logo} 
+              alt="Stevens Blueprint Logo" 
+              style={{ 
+                maxWidth: "200px"
+              }} 
+            />
+          </div></>, 
+        }}
       >
         {children}
       </AmplifyAuthenticator>
